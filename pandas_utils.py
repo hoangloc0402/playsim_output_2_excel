45167a23cfab7c92655342ab89922518a8caf5ce
Hoang Loc <nguyen.loc@gridsolar.jp>
2020-10-12 18:23:59 +0900

    add compiling kpi.yaml

--- pandas_utils.py
+++ pandas_utils.py
@@ -2,8 +2,55 @@ import os
 import pandas as pd
 from typing import Dict, List
 
+MISSING_VALUE = 'N/A'
 
-def config_2_dataframe(config_data: List[Dict])-> pd.DataFrame:
+
+def dicts_2_dataframe(data: List[Dict]) -> pd.DataFrame:
+    '''
+        Convert a list of dictionary into a dataframe, each dict
+            can have a different number of keys.
+        
+        All the dict values must be primitive, like this:
+        {
+            '5GO': 15600.0,
+            '7GO': 6928.2559690183025,
+            'JET': 49980.0,
+            'PG': 4098.38110238861,
+            ...
+        }
+        
+    '''
+    if len(data) == 0:
+        return None
+    
+    # Because each dict can have different number of attributes
+    # we need to merge them by first retrieving all the attribute names (keys)
+    all_keys = list()
+    # TODO: this code is really inefficient
+    for dictionary in data:
+        for key in dictionary.keys():
+            if key not in all_keys:
+                all_keys.append(key)
+
+    column_labels = ['case No.'] + all_keys
+
+    pd_data = list()
+    for index, dictionary in enumerate(data):
+        values = [index + 1, ]
+        for key in all_keys:
+            if key in dictionary.keys():
+                values.append(dictionary[key])
+            else:
+                values.append(MISSING_VALUE)
+        pd_data.append(values)
+    
+    df = pd.DataFrame(
+        data = pd_data,
+        columns = column_labels,
+    )
+    return df
+
+def config_2_dataframe(config_data: List[Dict]) -> pd.DataFrame:
     '''
         Convert data from all config.yaml files into 1 DataFrame object
         
@@ -15,32 +62,39 @@ def config_2_dataframe(config_data: List[Dict])-> pd.DataFrame:
                 config.yaml file
     
     '''
-    if len(config_data) == 0:
-        raise ValueError('No data to convert!')
-
-    column_labels = ['index'] + list(config_data[0].keys())
-
-    pd_data = list()
-    for index, data in enumerate(config_data):
-        values = [index] + list(data.values())
-        pd_data.append(values)
-    
-    df = pd.DataFrame(
-        data = pd_data,
-        columns = column_labels,
-    )
-    return df
+    return dicts_2_dataframe(config_data)
 
 
-def kpi_2_dataframe(kpi_data: List[Dict])-> pd.DataFrame:
+def kpi_2_dataframes(kpi_data: List[Dict])-> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
     '''
-        Convert data from all kpi.yaml files into 1 DataFrame object
+        Convert data from all kpi.yaml files into DataFrame objects
         
         Args:
             kpi_data (List[Dict]): each dictionary contains data of one
                 kpi.yaml file
-    
+
+        Returns:
+            3 dataframe for KPI, Shipment Amount and Shipment Restriction
+                respectively.
+
     '''
-    pass
+    list_kpi, list_shipment_amount, list_shipment_restriction = list(), list(), list()
+
+    for data in kpi_data:
+        list_kpi.append(dict())
+        for key, val in data.items():
+            if isinstance(val, (dict, list)):
+                if key == 'per_oil_amt':
+                    list_shipment_amount.append(val)
+                elif key == 'kisei':
+                    list_shipment_restriction.append(val)
+            else:
+                list_kpi[-1][key] = val
+    
+    df_kpi = dicts_2_dataframe(list_kpi)
+    df_shipment_amount = dicts_2_dataframe(list_shipment_amount)
+    df_shipment_restriction = dicts_2_dataframe(list_shipment_restriction)
+    
+    return df_kpi, df_shipment_amount, df_shipment_restriction
     
     
--- yaml_utils.py
+++ yaml_utils.py
@@ -1,4 +1,5 @@
 import os
+import re
 import yaml
 from typing import Dict, List, Generator
 
@@ -20,11 +21,11 @@ class YAML_Data():
     def __init__(self) -> None:
         self.config = list()
         self.kpi = list()
-        
+
     def add(self, config_data: Dict, kpi_data: Dict) -> None:
         self.config.append(config_data)
         self.kpi.append(kpi_data)
-    
+
     def is_empty(self) -> bool:
         return len(self.config) == len(self.kpi) == 0
 
@@ -35,77 +36,72 @@ def load_yaml_file(path: str) -> Dict:
             the provided path and return it as a dictionary
 
     '''
-    with open(path, 'r') as stream:
-        try:
+    try:
+        with open(path, 'r') as stream:
             data = yaml.safe_load(stream)
             if not isinstance(data, dict):
-                raise ValueError('Wrong file format!')
+                raise ValueError(f'{path} has unsupported format!')
             return data
 
-        except yaml.YAMLError as e:
-            print(e)
+    except yaml.YAMLError as e:
+        print(e)
 
 
-def list_all_public_dirs(path: str) -> List[str]:
+def list_all_public_dirs(path: str, sort=False) -> List[str]:
     '''
+        List all public directories and sort them by their creation time
+
         Args:
-            path (str): root path
+            path(str): root path
+            sort(bool): if True, sort the list of directory.
 
         Returns:
             dirs (List[str]): list containing all names of public
                 subdirectories inside the root directory.
 
     '''
+
+    def sorted_alphanumeric(data):
+        convert = lambda text: int(text) if text.isdigit() else text.lower()
+        alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)] 
+        return sorted(data, key=alphanum_key)
+
     dirs = list()
     for dir_entry in os.scandir(path):
         if not dir_entry.is_dir() or dir_entry.name.startswith("."):
             continue
         dirs.append(dir_entry.name)
+    if sort:
+        dirs = sorted_alphanumeric(dirs)
     return dirs
 
 
-def get_yaml_data_from_exec_dir(path: str) -> YAML_Data:
+def get_yaml_data_from_branch_dir(path: str) -> YAML_Data:
     '''
-        Read all config and kpi files inside one execution
+        Read all config and kpi files inside one branch
             directory
 
         Args:
-            path(str): path of an execution directory
+            path(str): path of an branch directory
         
     '''
+    print(f'LOADING BRANCH {path}/: START!')
     yaml_data = YAML_Data()
-    for root, _, files in os.walk(path):
-        if CONFIG_FILE not in files or KPI_FILE not in files:
+    count = 1
+    for sub_dir in list_all_public_dirs(path, sort=True):
+        config_path = os.path.join(path, sub_dir, CONFIG_FILE)
+        kpi_path = os.path.join(path, sub_dir, KPI_FILE)
+        if not os.path.exists(config_path) or not os.path.exists(kpi_path):
+            print(f'\t    Skipped .../{sub_dir}/')
             continue
-        config_data = load_yaml_file(
-            os.path.join(root, CONFIG_FILE))
-        kpi_data = load_yaml_file(
-            os.path.join(root, KPI_FILE))
+        print(f'\t[{count}] Reading .../{sub_dir}/')
+        count += 1 
+        config_data = load_yaml_file(config_path)
+        kpi_data = load_yaml_file(kpi_path)
         yaml_data.add(config_data, kpi_data)
 
+    if yaml_data.is_empty():
+        print(f'LOADING BRANCH {path}/: NO DATA!\n')
+    else:
+        print(f'LOADING BRANCH {path}/: DONE!\n')
     return yaml_data
-
-
-def iterate_yaml_data_from_root(path: str) -> Generator[str, YAML_Data]:
-    '''
-        Load data by going through all execution directories
-
-        Args:
-            path(str): root path, the directory which contains multiple
-                execution directories
-        
-        Returns:
-            A tuple of root_path, name of the execution directory
-                and an YAML_Data object
-        
-    '''
-    for exec_dir in list_all_public_dirs(path):
-        print(f'LOADING FROM .../{exec_dir}/: START!')
-        exec_dir_abs_path = os.path.join(path, exec_dir)
-        yaml_data = get_yaml_data_from_exec_dir(exec_dir_abs_path)
-
-        if yaml_data.is_empty():
-            print(f'LOADING FROM .../{exec_dir}/: NO DATA!')
-            continue
-        print(f'LOADING FROM .../{exec_dir}/: DONE!')
-        yield exec_dir, yaml_data
